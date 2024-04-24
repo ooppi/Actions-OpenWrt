e1f5e4b7b30bd5e61d2cc5b4d71839f9a7475430
ooppi <49905278+ooppi@users.noreply.github.com>
2024-04-24 20:17:00 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,5 +1,5 @@
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.5.86/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.5.30/g' package/base-files/files/bin/config_generate
 
 # Modify hostname
 #sed -i 's/OpenWrt/Kurty-Router/g' package/base-files/files/bin/config_generate
