a21899546615d9b24be3ed036ab694d836f85589
ooppi <49905278+ooppi@users.noreply.github.com>
2025-04-05 11:47:27 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,5 +1,5 @@
 
-sed -i 's/192.168.1.1/192.168.5.30/g' package/base-files/luci2/bin/config_generate          # 修改IP地址为192.168.0.55
+sed -i 's/192.168.1.1/192.168.5.86/g' package/base-files/luci2/bin/config_generate          # 修改IP地址为192.168.0.55
 
 # Modify hostname
 sed -i 's/OpenWrt/Kurty-Router/g' package/base-files/luci2/bin/config_generate 
